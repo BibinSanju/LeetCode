8f9339809452c22737ebb40b7844aed3f50cd85a
BibinSanju <bibinsanju18@gmail.com>
2026-05-27 21:02:11 +0530

    Time: 64 ms (22.33%), Space: 19.6 MB (42.88%)

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        def isPali(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left+=1
+                right-=1
+            return True
+
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                if isPali(left+1,right) or isPali(left, right-1):
+                    return True
+                else:
+                    return False
+            left+=1
+            right-=1
+        return True
