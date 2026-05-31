e91d0bb54309321efea55a8a0946e27e8feb0261
BibinSanju <bibinsanju18@gmail.com>
2026-05-31 20:20:24 +0530

    Time: 1 ms (96.03%), Space: 19.2 MB (82.22%)

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_t, t_s = {},{}
+        for ch1,ch2 in zip(s,t):
+            if ch1 in s_t:
+                if s_t[ch1] != ch2:
+                    return False
+            else:
+                s_t[ch1] = ch2
+            
+            if ch2 in t_s:
+                if t_s[ch2] != ch1:
+                    return False
+            else:
+                t_s[ch2] = ch1
+        
+        return True
