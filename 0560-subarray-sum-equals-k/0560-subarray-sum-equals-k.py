c5117772a2059c192ba34525250916e5c53be7ee
BibinSanju <bibinsanju18@gmail.com>
2026-06-03 21:31:59 +0530

    Time: 34 ms (57.48%), Space: 21.8 MB (57.06%)

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,5 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+            not a big deal, so for o(n) time, I look from a index i, is there some 
+            portion or subarray from 0 to i , I can chop off to achive the target subarray,
+            so i store prefix sum (counts), so when I stand in index i, i how the count  of 
+            subarray (who has a sum i need to chop to get k), there may be many subarray with 
+            same value (i.e 1), I can chop , so I gets the total sub array which will be == 
+            the count of prefix sum i delete
+        """
         ans = 0
         cum_sum = 0
         prefix_map = { 0 : 1 }
