a1389c06bf08ee3c27dd730be46cac3809a98fc4
BibinSanju <bibinsanju18@gmail.com>
2026-06-03 22:11:32 +0530

    Time: 52 ms (59.94%), Space: 39.3 MB (91.33%)

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,5 +1,12 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        """
+        So, Here it is same as prefix um app. o(n), where we store the remainder as key 
+        and index as val ( so using ind we can find whether the subarray has >2 elems),
+        Trick is, to some subarray with rem r, we add another subarray which is mutiple ok K
+        (f its multiple of K then rem == 0), we get the rem r again, so we know we added a sol, use ind diff to is it a good array, we add {0:-1}, [0,0] is a valid subarray 
+
+        """
         remainder_map = { 0: -1}
         cum_sum = 0
 
