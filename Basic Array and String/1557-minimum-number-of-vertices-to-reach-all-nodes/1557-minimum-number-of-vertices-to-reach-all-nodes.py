8766066eea72473a6278c1e6af909eb7f577c019
BibinSanju <bibinsanju18@gmail.com>
2026-07-30 22:32:58 +0530

    Time: 15 ms (64.86%), Space: 45.54 MB (75.93%)

---
+++ Basic Array and String/1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        in_deg = [0] * n
+        for u,v in edges:
+            in_deg[v] += 1
+        
+        return list(i for i in range(n) if in_deg[i] == 0)
---
+++ Basic Array and String/1557-minimum-number-of-vertices-to-reach-all-nodes/README.md
@@ -0,0 +1,4 @@
+It's a DAG, so if a vertex has no i degree we need to start from that vertex to include it, so in_deg == 0 vertices are the ans
+
+TC = $$ O(E+V) $$
+SC = $$ O(V) $$
